2b1434edf43e12eb09d14bf1f76a5b1ce15cf7a8
Ziang Li <ziangli@umich.edu>
2023-06-22 00:06:16 -0400

    Minimal setup

--- CNS/Exec/Sod/run_gl.sh
+++ CNS/Exec/Sod/run_gl.sh
@@ -5,8 +5,8 @@
 
 #SBATCH --job-name=run_cns_sod
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=4
-#SBATCH --mem-per-cpu=8000m
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=2048m
 #SBATCH --time=05:00
 #SBATCH --account=ramanvr0
 #SBATCH --partition=gpu
@@ -14,4 +14,4 @@
 
 module load cuda gcc openmpi
 
-nsys profile mpirun -np 4 CNS3d.gnu.TPROF.MPI.CUDA.ex inputs
+nsys profile mpirun -np 1 CNS3d.gnu.TPROF.MPI.CUDA.ex inputs
