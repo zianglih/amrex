5337d3124d0e1a83f9099003c02ba3ce2335bf2b
Ziang Li <ziangli@umich.edu>
2023-06-23 15:42:11 -0400

    Figured out mutiple CPUs GPUs

--- CNS/Exec/Sod/run_gl.sh
+++ CNS/Exec/Sod/run_gl.sh
@@ -4,18 +4,25 @@
 #“#SBATCH” directives that convey submission options:
 
 #SBATCH --job-name=run_cns_sod
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem-per-cpu=2048m
 #SBATCH --time=05:00
 #SBATCH --account=ramanvr0
 #SBATCH --partition=gpu
-#SBATCH --gpus-per-node=1
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --gpus-per-node=2
+#SBATCH --mem-per-cpu=8g
+#SBATCH --gpu-bind=map_gpu:0,1
+
+module purge
 
 module load cuda cupti gcc openmpi
 
-nsys profile --trace=cuda,nvtx,osrt --backtrace=dwarf --cuda-memory-usage=true mpirun -np 1 CNS3d.gnu.TPROF.MPI.CUDA.ex inputs
-# nvprof mpirun -np 1 CNS3d.gnu.TPROF.MPI.CUDA.ex inputs
-# nsys profile -c nvtx -p "main@*" -e NSYS_NVTX_PROFILER_REGISTER_ONLY=0 --trace=cuda,nvtx,osrt --backtrace=dwarf --gpu-metrics-device=all --gpu-metrics-frequency=20000 mpirun -np 1 CNS3d.gnu.TPROF.MPI.CUDA.ex inputs
-# mpirun -np 1 ncu -o kernels --target-processes all -c 2 CNS3d.gnu.TPROF.MPI.CUDA.ex inputs amrex.fpe_trap_invalid=0
-# ncu -k 5 --target-processes all CNS3d.gnu.TPROF.MPI.CUDA.ex inputs amrex.fpe_trap_invalid=0
+# For MPI:
+# nsys profile --trace=cuda,nvtx,osrt,mpi --cuda-memory-usage=true mpirun -np 2 CNS3d.gnu.TPROF.MPI.CUDA.ex inputs
+
+# For a single CPU & GPU:
+# nsys profile --trace=cuda,nvtx,osrt --cuda-memory-usage=true mpirun -np 1 CNS3d.gnu.TPROF.MPI.CUDA.ex inputs
+
+# For Nsight Compute:
+ncu --nvtx --nvtx-include "main()" --target-processes all CNS3d.gnu.TPROF.MPI.CUDA.ex inputs amrex.fpe_trap_invalid=0
