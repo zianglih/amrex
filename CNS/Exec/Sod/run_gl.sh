69567e6076f17aac566e7a4eed1c6d51fead8333
Ziang Li <ziangli@umich.edu>
2023-06-23 13:00:59 -0400

    More profiling work

--- CNS/Exec/Sod/run_gl.sh
+++ CNS/Exec/Sod/run_gl.sh
@@ -12,6 +12,10 @@
 #SBATCH --partition=gpu
 #SBATCH --gpus-per-node=1
 
-module load cuda gcc openmpi
+module load cuda cupti gcc openmpi
 
-nsys profile mpirun -np 1 CNS3d.gnu.TPROF.MPI.CUDA.ex inputs
+nsys profile --trace=cuda,nvtx,osrt --backtrace=dwarf --cuda-memory-usage=true mpirun -np 1 CNS3d.gnu.TPROF.MPI.CUDA.ex inputs
+# nvprof mpirun -np 1 CNS3d.gnu.TPROF.MPI.CUDA.ex inputs
+# nsys profile -c nvtx -p "main@*" -e NSYS_NVTX_PROFILER_REGISTER_ONLY=0 --trace=cuda,nvtx,osrt --backtrace=dwarf --gpu-metrics-device=all --gpu-metrics-frequency=20000 mpirun -np 1 CNS3d.gnu.TPROF.MPI.CUDA.ex inputs
+# mpirun -np 1 ncu -o kernels --target-processes all -c 2 CNS3d.gnu.TPROF.MPI.CUDA.ex inputs amrex.fpe_trap_invalid=0
+# ncu -k 5 --target-processes all CNS3d.gnu.TPROF.MPI.CUDA.ex inputs amrex.fpe_trap_invalid=0
